6ae5c0f4bdb88bcfa26842345d52c586852cd468
Dimitri Mockelyn <dimitri.mockelyn@gmail.com>
2014-05-26 19:55:13 +0100

    first test : everything hard encoded

---
+++ first_get.php
@@ -0,0 +1,51 @@
+<?php
+$steamkey= '69E4634F40383769D291FC9BF5F1C5FE';
+$steamid = '76561198120359242.76561198120359242'; //get_steam_id();
+$requete =  'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/';
+$requete = $requete.'?key='.$steamkey.
+			'&steamids='.$steamid;			
+
+$ch = curl_init($requete);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$resultat_request = curl_exec($ch);
+
+if (!$resultat_request)
+	{echo "<p>Erreur</p><p>".curl_error($ch)."</p>";}
+
+else 
+{
+	$liste_parametres = explode("&",$resultat_request);
+	$counter = 0;
+	foreach($liste_parametres as $param_steam)
+	{
+		$list_param_steam=json_decode($param_steam);
+		foreach($list_param_steam as $response)
+		{
+			foreach ($response as $player ) {
+				$liste_param_steam=$player;
+			};
+		} 
+//		list($name,$valeur) = explode("=",$param_steam);
+//		$liste_param_steam[$name]=urldecode($valeur);
+/*		foreach($response as $list_players) 
+		{
+			foreach($list_players as $players) 
+			{
+				list($name,$valeur) = explode(":",$players);
+				$liste_param_players[$name]=urldecode($valeur);
+			}
+			$liste_param_steam[$players]=$liste_param_players;
+		}
+*/		
+	}
+	echo "<pre>";
+	print_r($liste_param_steam);
+	echo "</pre>";
+}
+
+curl_close($ch);
+
+?>
