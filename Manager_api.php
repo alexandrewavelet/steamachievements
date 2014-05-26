1da7a834b479b2e85428edfd1e517e653026acf8
Alexandre Wavelet <wavelet.alexandre@gmail.com>
2014-05-26 22:22:45 +0200

    index form

--- Manager_api.php
+++ Manager_api.php
@@ -17,16 +17,15 @@
 			$request = 'http://steamcommunity.com/id/'.$username.'/?xml=1';
 
 			$resultat_request = $this->getCURL($request);
-			if (!$resultat_request)
+			if (!$resultat_request['code'])
 			{
-				$reponse = array('code' => 0, 'message' => 'Erreur : '.curl_error($ch));
+				$reponse = array('code' => 0, 'message' => 'Erreur : '.$resultat_request['message']);
 			}
 			else 
 			{
-				preg_match("#<steamID64>[0-9]+#",$resultat_request,$match);
+				preg_match("#<steamID64>[0-9]+#",$resultat_request['message'],$match);
 				$reponse = array('code' => 1, 'message' => $match[0]);
 			}
-			curl_close($ch);
 			return $reponse;
 		}
 
@@ -36,6 +35,16 @@
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			$resultat_request = curl_exec($ch);
+			if (!$resultat_request)
+			{
+				$reponse = array('code' => 0, 'message' => curl_error($ch));
+			}
+			else
+			{
+				$reponse = array('code' => 1, 'message' => $resultat_request);
+			}
+			curl_close($ch);
+			return $reponse;
 		}
 
 	}
