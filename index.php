8fc05f1384a8d686fe54025fb8a765582153b6b6
Dimitri Mockelyn <dimitri.mockelyn@gmail.com>
2014-05-26 20:46:41 +0100

    bug fixes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <h1>index</h1>
 
 <ul>
-	<li><a href="first_get.php">first get</a></li>
+	<li><a href="get_profile_from_username.php">get profile from username</a></li>
 	<li><a href="get_steamid_from_username.php">get steamid from username</a></li>
 </ul>
